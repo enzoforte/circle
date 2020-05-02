0da5fec758088e9573310a51573ad560f2d5769c
Enzo Forte <v.forte6@gmail.com>
2020-05-02 20:48:58 +0200

    add __init__

--- Src/app.py
+++ Src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
@@ -6,3 +7,18 @@ app = Flask(__name__)
 def index():
     return "hello world"
 
+@app.route('/covid/stats')
+def stats():
+
+    url = "https://covid-193.p.rapidapi.com/statistics"
+
+    querystring = {"country":"italy"}
+
+    headers = {
+        'x-rapidapi-host': "covid-193.p.rapidapi.com",
+        'x-rapidapi-key': "7c58847555mshbfa8d8a06d2cfb1p138208jsncf4c82087ba7"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    return response.text
