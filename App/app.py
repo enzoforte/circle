ed82e4d6edbbf6bed423776c311b03fba20f25a7
Enzo Forte <v.forte6@gmail.com>
2020-05-10 18:54:10 +0200

    add setup

--- App/app.py
+++ App/app.py
@@ -1,26 +1,14 @@
 from flask import Flask
 import requests
-from Src.Circle import Circle
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     #circle = Cicle()
-    return "circle"
+    pass
 
-@app.route('/covid/stats')
+@app.route('/test')
 def stats():
-
-    url = "https://covid-193.p.rapidapi.com/statistics"
-
-    querystring = {"country":"italy"}
-
-    headers = {
-        'x-rapidapi-host': "covid-193.p.rapidapi.com",
-        'x-rapidapi-key': "7c58847555mshbfa8d8a06d2cfb1p138208jsncf4c82087ba7"
-        }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    return response.text
+    pass
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='mymodule',
+  use_scm_version=True,
+  setup_requires=[
+    'setuptools_scm',
+  ],
+  packages=find_packages(exclude=['Test']),
+  install_requires=[
+    'flask==0.12',
+    'six==1.10.0',
+  ],
+)
